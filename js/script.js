908645a637ec1ced1ac66714c8f61447d66aa4b2
mariaandrezacs <mandreza008@gmail.com>
2024-08-05 11:39:30 -0300

    feat: ajustes

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Função para carregar o conteúdo de um arquivo HTML
+function loadHTML(elementId, filePath) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Erro ao carregar o HTML:', error);
+        });
+}
+
+// Carregar os templates
+loadHTML('header', IDBIndex('login'));
+loadHTML('footer', '');
