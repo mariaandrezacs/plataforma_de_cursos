af5c738830aae33be5973697793ff8ac8fe33f7d
mariaandrezacs <mandreza008@gmail.com>
2024-08-22 13:17:06 -0300

    feat: ajuste layout

--- js/muda_cor.js
+++ js/muda_cor.js
@@ -3,9 +3,21 @@ function mudarCor(id) {
     // Remove a classe 'active' de qualquer outro elemento
     document.querySelectorAll('.conteudo').forEach(function(el) {
         el.classList.remove('active');
+        
     });
+    
 
 // Adiciona a classe 'active' ao conteúdo desejado
 document.getElementById(id).classList.add('active');
 
+}
+
+
+function scrollToContent(id) {
+    var element = document.getElementById(id);
+    element.scrollIntoView({ 
+        behavior: 'smooth', 
+        block: 'center'
+    });
+    mudarCor(id); // Chama sua função de mudança de cor
 }
